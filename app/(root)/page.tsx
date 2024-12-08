7bbeb7eb6c4594b732a6cc778fabcd642fd81d45
Viet An Nguyen <vietannguyen042002@gmail.com>
2024-12-08 12:24:10 +1030

    Save to fix errors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,25 @@
-
+// import CategoryFilter from '@/components/shared/CategoryFilter';
+import Collection from '@/components/shared/Collection'
+import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
+import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const params = await searchParams;
+  const page = Number(params?.page) || 1;
+  const searchText = (params?.query as string) || '';
+  const category = (params?.category as string) || '';
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -27,6 +43,25 @@ export default async function Home() {
           />
         </div>
       </section> 
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          {/* <CategoryFilter /> */}
+        </div>
+
+        <Collection 
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
+        />
+      </section>
     </>
   )
 }
